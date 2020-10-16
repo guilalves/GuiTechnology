a917c52c4ca4d2a67974f36be0623d6428e2d340
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-15 23:31:47 -0300

    ajustes

--- aplicacao/GuiTechnology/urls.py
+++ aplicacao/GuiTechnology/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls.static import static
 from . import views
 
-urlspatterns = [
+urlpatterns = [
     path('', views.index, name='index')
 ]

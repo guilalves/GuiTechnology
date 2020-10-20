ad404a8e689d58ef93918a108aecfe9c8f9f04f6
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-19 23:11:24 -0300

    separando e organizando a pagina principal e secundaria e criando arquivos separados p/ melhor organizacao

--- aplicacao/GuiTechnology/urls.py
+++ aplicacao/GuiTechnology/urls.py
@@ -9,5 +9,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('produto', views.produto, name='produto')
 ]
--- aplicacao/GuiTechnology/views.py
+++ aplicacao/GuiTechnology/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+def produto(request):
+    return render(request,'produtos.html')

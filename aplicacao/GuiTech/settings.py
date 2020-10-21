a20500e4744bf9585f61fa8f27d228f3574d5d77
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-20 23:28:57 -0300

    configurando banco de dados

--- aplicacao/GuiTech/settings.py
+++ aplicacao/GuiTech/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os, sys
+import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'GuiTech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'produtos',
+        'USER': 'postgres',
+        'PASSWORD': '529440',
+        "HOST": 'localhost'
     }
 }
 
--- aplicacao/GuiTechnology/models.py
+++ aplicacao/GuiTechnology/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class produto(models.Model):
+    nome_produto = models.CharField(max_length=200)
+    detalhes = models.TextField()
+    preco = models.TextField()
+    garantia = models.IntegerField()
+    date_produto = models.DateTimeField(default=datetime.now, blank=True)

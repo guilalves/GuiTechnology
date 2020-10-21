3878204fe73fdd0ec4afbee185d7a77debdeae1c
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-20 21:32:09 -0300

    adicionando nome dos produtos no template dinamicamente

--- aplicacao/GuiTechnology/views.py
+++ aplicacao/GuiTechnology/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,'index.html')
+    
+    produtos = {
+        1:'Suporte para notebook',
+        2:'MousePad',
+        3:'Cabo HDMI',
+        4:'Adaptador de VGA'
+    }
+    dados = {
+        'nome_dos_produtos' : produtos
+    }
+    return render(request,'index.html',dados)
 
 def produto(request):
     return render(request,'produtos.html')

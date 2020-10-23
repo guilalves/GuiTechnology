db2c6147da644385ed854ac5007bac5429350ee6
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-23 18:29:21 -0300

    integrando modelos

--- aplicacao/GuiTechnology/models.py
+++ aplicacao/GuiTechnology/models.py
@@ -1,7 +1,10 @@
+import pessoas
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 class Produto(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_produto = models.CharField(max_length=200)
     detalhes = models.TextField()
     preco = models.CharField(max_length=200)

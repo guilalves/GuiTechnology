8827b72ab3796d39b81fd1b6a12d02879e6789da
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-23 17:48:50 -0300

    otimizando as buscas, filtros e paginacao

--- aplicacao/GuiTechnology/admin.py
+++ aplicacao/GuiTechnology/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Produto
 
-admin.site.register(Produto)
+class ListandoProdutos(admin.ModelAdmin):
+    list_display = ('id', 'nome_produto', 'marca', 'preco')
+    list_display_links = ('id', 'nome_produto')
+    search_fields = ('id', 'nome_produto')
+    list_filter = ('marca',)
+    list_per_page = (6)
+
+admin.site.register(Produto, ListandoProdutos)

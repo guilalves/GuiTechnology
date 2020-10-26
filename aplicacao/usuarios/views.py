6bd1a8015dd40b16e0fdd569f66f3286e04774aa
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-26 15:08:03 -0300

    Realizando autenticação e login

--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == 'POST':
@@ -35,8 +36,13 @@ def login(request):
         if email == "" or senha == "":
             print('OS CAMAPOS NÃO PODE FICAR EM BRANCO')
             return redirect('login')
-        print(email, senha)
-        return redirect('dashboard')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('dashboard')
     return render(request,'usuarios/login.html')
 
 def logout(request):

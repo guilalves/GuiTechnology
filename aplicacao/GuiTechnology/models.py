88d165866460ac0ea010b2c453b9eb6a4a15a23e
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-25 22:22:56 -0300

    exibindo foto do produto

--- aplicacao/GuiTechnology/models.py
+++ aplicacao/GuiTechnology/models.py
@@ -10,3 +10,4 @@ class Produto(models.Model):
     preco = models.CharField(max_length=200)
     date_produto = models.DateTimeField(default=datetime.now, blank=True)
     marca = models.CharField(max_length=200)
+    foto_produto = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)

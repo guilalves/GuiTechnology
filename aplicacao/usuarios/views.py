b75b1ac25a8029d7be09a40ac424ed281f552c20
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-26 13:41:15 -0300

    Inserindo as Requisições no Django

--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -1,8 +1,12 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def cadastro(request):
-    return render(request,'usuarios/cadastro.html')
+    if request.method == 'POST':
+        print('ok')
+        return redirect('login')
+    else:
+        return render(request,'usuarios/cadastro.html')
 
 def login(request):
     return render(request,'usuarios/login.html')

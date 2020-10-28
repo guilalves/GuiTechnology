24e2756bc4850f8f16fe395f09892035c1d0c6a1
guilherme ferreira <guilherme.alves96@hotmail.com>
2020-10-27 23:10:51 -0300

    ajustes

--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -1,6 +1,6 @@
 from django.contrib.messages.api import error
 from django.http import request
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from GuiTechnology.models import Produto
